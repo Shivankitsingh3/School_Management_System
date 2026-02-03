776de1c1d4fcf183d5f97ccf64eda0414d9657f7
Shivankit <shivankitsingh3@gmail.com>
2026-02-03 22:55:55 +0530

    add account initial migration and sqlite fallback

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -93,9 +93,20 @@ ASGI_APPLICATION = 'core.asgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
-DATABASES = {
-    'default': dj_database_url.parse(os.getenv("DATABASE_URL"))
-}
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+if DATABASE_URL:
+    DATABASES = {
+        'default': dj_database_url.parse(DATABASE_URL)
+    }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
+
 
 DATABASES['default']['CONN_MAX_AGE'] = 600
 

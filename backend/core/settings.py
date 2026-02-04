0a5fcb00f26729ecb1d301b2fe9e04c83c9c8908
Shivankit <shivankitsingh3@gmail.com>
2026-02-04 13:29:04 +0530

    Fix: Remove trailing slash from CORS_ALLOWED_ORIGINS

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -179,7 +179,7 @@ REST_FRAMEWORK = {
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:5173',
     'http://127.0.0.1:5173',
-    'https://school-management-system-pi-lac.vercel.app/',
+    'https://school-management-system-pi-lac.vercel.app',
 ]
 
 CORS_ALLOW_CREDENTIALS = True
